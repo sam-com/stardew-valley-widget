e70a92011bb6dc3c5976252ff25e37b51b295560
sam-com <samuelcomeau@gmail.com>
2024-12-13 19:46:24 -0500

    fix bg

--- src/lib/Background.svelte
+++ src/lib/Background.svelte
@@ -2,48 +2,40 @@
 	import { blur } from 'svelte/transition';
   import { cubicInOut } from 'svelte/easing';
 
-  // import bgDay from "@assets/bg_day.svg";
-  // import bgNight from "@assets/bg_night.svg";
   import winterDay from "@assets/bg/winter_day.png";
   import winterNight from "@assets/bg/winter_night.png";
   import sprintDay from "@assets/bg/spring_day.png";
   import springNight from "@assets/bg/spring_night.png";
+  import summerDay from "@assets/bg/summer_day.png";
+  import summerNight from "@assets/bg/summer_night.png";
+  import fallDay from "@assets/bg/fall_day.png";
+  import fallNight from "@assets/bg/fall_night.png";
   
-  import { getDateInterval } from "@components/getDateInterval.svelte";
-  import { isNightTime } from "@utils/daylight";
-  import { SvelteDate } from 'svelte/reactivity';
-  import { untrack } from 'svelte';
-
-  const allBg = [winterDay,winterNight,sprintDay,springNight];
+  const allBackgrounds = [winterDay,winterNight,sprintDay,springNight,summerDay,summerNight,fallDay,fallNight];
 
   let { daylight } = $props();
-  let currentDate = getDateInterval(2000);
-  let previous = $state(false);
-  let idx = $state(0);
-  // let isNight = $derived(isNightTime({current, ...daylight}));
-  let isNight = $derived.by(() => {
-    if(currentDate.getTime()) {
-      // previous = !previous;
-      untrack(() => {
-      previous = !previous;
-      idx = idx + 1 % allBg.length});
-      return allBg[idx]
-    }
+  let current = $state(0);
+
+  $inspect(current)
+
+  $effect(() => {
+    const handle = setInterval(() => {
+      current = (current + 1) % allBackgrounds.length;
+    },3000);
+
+    return () => {clearInterval(handle)}
   })
 
 </script>
 
 <div class="backgroundContainer">
-  {#if previous}
-    {@render background(isNight)}
-  {:else}
-    {@render background(isNight)}
-  {/if}
+  {#each allBackgrounds as bg,idx}
+    {#if idx === current}
+      <img alt="background" class="background" src={bg} transition:blur={{easing:cubicInOut, duration:2000 }} />
+    {/if}
+  {/each}
 </div>
 
-{#snippet background(src)}
-  <img alt="background" class="background" src={src} transition:blur={{easing:cubicInOut, duration:2000 }} />
-{/snippet}
 
 <style>
   .backgroundContainer {
